b1e32bbd080085f7d047a962ce69b6cb7b2b43ec
MoonCheung <ikmoons.is@gmail.com>
2023-03-19 21:31:40 +0800

    chore: update config

--- playground/react/vite.config.ts
+++ playground/react/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // we alias to the lib's source files in dev
+      // so we don't need to rebuild the lib over and over again
+      '@img-uploader/react':
+        process.env.NODE_ENV === 'production' ? '@img-uploader/react' : '@img-uploader/react/src/index.ts'
+    },
+    dedupe: ['react']
+  },
+  server: {
+    port: 8081
+  },
+  optimizeDeps: {
+    exclude: ['@img-uploader/react']
+  }
+});
--- playground/vue/vite.config.ts
+++ playground/vue/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJSX from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), vueJSX()],
+  resolve: {
+    alias: {
+      // we alias to the lib's source files in dev
+      // so we don't need to rebuild the lib over and over again
+      '@img-uploader/vue':
+        process.env.NODE_ENV === 'production' ? '@img-uploader/vue' : '@img-uploader/vue/src/index.ts'
+    },
+    dedupe: ['vue']
+  },
+  server: {
+    port: 8080
+  },
+  optimizeDeps: {
+    exclude: ['@img-uploader/vue']
+  }
+});
